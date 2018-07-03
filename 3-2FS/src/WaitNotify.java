ee54f2a84dda5f3e84ba9de12d51a1a75b9a54c3
Priyanker <priyankerrao@gmail.com>
2018-07-04 00:40:44 +0530

    Added wait&notify producer-consumer program

--- 3-2FS/src/WaitNotify.java
+++ 3-2FS/src/WaitNotify.java
@@ -98,7 +98,7 @@ class WaitNotify
         MyRun2 r2 = new MyRun2(d);
         Thread t1 = new Thread(r1);
         Thread t2 = new Thread(r2);
-        t1.start();
         t2.start();
+        t1.start();
     }
 }
